093c1fdf5b87bc2cc020924548ed299bc8130b36
Connergifford <111781584+Connergifford@users.noreply.github.com>
2022-12-08 12:22:30 -0500

    Add files via upload

---
+++ Combination.py
@@ -0,0 +1,51 @@
+#00089348
+
+import random
+
+get_numbers_of_dials = "How many dials do you want in this lock? "
+print(input(get_numbers_of_dials))
+
+if get_numbers_of_dials : 2
+print("Sorry i can only have 3 or more dials. ")
+than:print(input(get_numbers_of_dials))
+    
+
+def letter():
+
+    a = 0
+b = 1
+c = 2
+d = 3
+e = 4
+f = 5
+g = 6
+h = 7
+i = 8
+j = 9
+
+def b():
+
+    if letter : b
+    print(1)
+
+def d():
+
+    if letter : d
+    print(3)
+
+def g():
+
+    if letter : g
+    print(6)
+
+
+combination = "What's the combination that you want? "
+print(input(combination))
+
+nub = input
+
+numb = random
+{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+if numb == 1:
+    print(1 == numb * 3)
